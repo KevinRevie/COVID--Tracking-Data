9f4428d367cce005dfd0c5e06441d366c452efcf
KevinRevie <kevin_revie@hotmail.com>
2020-06-23 10:48:40 -0700

    KLR: Added Facebook tags, new Infection Totals report

--- js/base.js
+++ js/base.js
@@ -8,3 +8,19 @@ function formatNumber(numbertoformat) {
     returnnumber = returnnumber.replace('.000', '');
     return returnnumber;
 }
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+$(document).ready(function () {
+    var url = window.location;
+    $('.fb-share-button').attr('data-href', url);
+    //console.log('test:'+url);
+});
